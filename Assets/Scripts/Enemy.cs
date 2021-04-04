6f6b37aa661082cf10afd6728e952a968ef6b9f1
EricBL3 <ericbuitron3@gmail.com>
2021-04-04 16:41:49 -0500

    Add states and transitions to Enemy Script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,43 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float CurrentHealth;
 
+    private StateMachine stateMachine;
+
+    public GameObject Target;
+
+    private void Awake()
+    {
+        //Get all necessary components
+
+        stateMachine = new StateMachine();
+
+        //States
+        var patrol = new Patrol();
+        var chase = new Chase();
+        var attack = new Attack();
+        var die = new Die();
+
+        //Normal transitions
+        At(patrol, chase, InDetectionRange());
+        At(chase, patrol, OutOfDetectionRange());
+        At(chase, attack, InAttackRange());
+        At(attack, chase, OutOfAttackRange());
+
+        //Transitions that can happen at any time
+        stateMachine.AddAnyTransition(die, IsDead());
+
+        //Set initial state
+        stateMachine.SetState(patrol);
+
+        //Definitions
+        void At(IState from, IState to, Func<bool> condition) => stateMachine.AddTransition(from, to, condition);
+        //Add Func<bool> of conditions
+        Func<bool> InDetectionRange() => () => Vector3.Distance(transform.position, Target.transform.position) <= enemy.enemyData.DetectRange;
+        Func<bool> OutOfDetectionRange() => () => Vector3.Distance(transform.position, Target.transform.position) > enemy.enemyData.DetectRange;
+        Func<bool> InAttackRange() => () => Vector3.Distance(transform.position, Target.transform.position) <= enemy.enemyData.AttackRange;
+        Func<bool> OutOfAttackRange() => () => Vector3.Distance(transform.position, Target.transform.position) > enemy.enemyData.AttackRange;
+        Func<bool> IsDead() => () => CurrentHealth <= 0;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +59,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        stateMachine.Update();
     }
 }
