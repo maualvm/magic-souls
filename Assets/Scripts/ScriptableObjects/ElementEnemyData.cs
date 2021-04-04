9284f18a014d3f40e55fe88dcbd4e5751e18687c
EricBL3 <ericbuitron3@gmail.com>
2021-04-04 14:20:40 -0500

    Add ElementEnemyData and EnemyData ScriptableObjects.

---
+++ Assets/Scripts/ScriptableObjects/ElementEnemyData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemies/Element Enemy")]
+public class ElementEnemyData : ScriptableObject
+{
+
+    public EnemyData enemyData;
+    public string element;
+
+    public GameObject Model;
+}
