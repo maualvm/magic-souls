12d52b89b44866a14d87b7361ca3b794a8c55804
EricBL3 <ericbuitron3@gmail.com>
2021-04-04 14:21:08 -0500

    Add Enemy Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    ElementEnemyData enemy;
+ 
+    [SerializeField]
+    float CurrentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = enemy.enemyData.MaxHealth;
+        Debug.Log("This is a " + enemy.element + " " + enemy.enemyData.enemyType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
