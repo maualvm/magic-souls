260e2c912dce401d2bf1eed9552c4ac81dfbed19
EricBL3 <ericbuitron3@gmail.com>
2021-05-23 15:47:27 -0500

    Add Main Menu, Instructions and Credits scenes with transitions.

---
+++ Assets/Scripts/Scene Transitions/GoToMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/Scene Transitions/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Puebito");
+    }
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
