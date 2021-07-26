04d53497a99970a2a86a120427498d1dbc8261ba
EricBL3 <ericbuitron3@gmail.com>
2021-07-26 17:17:54 -0500

    Add volume settings for master, music and sfx

---
+++ Unity Project/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField]
+    string volumeParameter = "MasterVolume";
+    [SerializeField]
+    AudioMixer mixer;
+    [SerializeField]
+    Slider slider;
+    [SerializeField]
+    float multiplier = 30f;
+    [SerializeField]
+    Toggle toggle;
+    private bool disableToggleEvent;
+
+    private void Awake()
+    {
+        slider.onValueChanged.AddListener(HandleSliderValueChanged);
+        toggle.onValueChanged.AddListener(HandleToggleValueChanged);
+    }
+
+    private void HandleToggleValueChanged(bool enableSound)
+    {
+        AudioManager.PlaySound(AudioManager.Sound.Confirm);
+
+        if (disableToggleEvent)
+            return;
+
+        if(enableSound)
+            slider.value = slider.maxValue;
+        else
+            slider.value = slider.minValue;
+    }
+
+    private void HandleSliderValueChanged(float value)
+    {
+        mixer.SetFloat(volumeParameter, Mathf.Log10(value) * multiplier);
+        disableToggleEvent = true;
+        toggle.isOn = slider.value > slider.minValue;
+        disableToggleEvent = false;
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, slider.value);
+    }
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat(volumeParameter, slider.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
